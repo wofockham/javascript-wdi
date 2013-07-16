4b26487937d96fedd7ef283c12cf3adc52697d55
Cold Head <certainthings@gmail.com>
2013-07-16 15:59:54 +1000

    Paintr v0.3 - images, control key to draw

--- paintr/app/assets/javascripts/paintr.js
+++ paintr/app/assets/javascripts/paintr.js
@@ -8,19 +8,19 @@ $(document).ready(function () {
   };
   create_paint_boxes(5000);
 
-  var paint_box = function () {
+  var paint_box = function (event) {
+    if (! event.ctrlKey) return;
     var $box = $(this);
     var color = $('#current').css('background-color');
     $box.css('background-color', color);
   };
-  $('.paint').click(paint_box);
   $('.paint').mouseover(paint_box);
 
-  var clear_colors = function () {
-    $('#colors').empty();
+  var clear_all = function () {
+    $('#colors, #images').empty();
     $('#current').css('background-color', 'white');
   }
-  $('#clear').click(clear_colors);
+  $('#clear').click(clear_all);
 
 
   var add_color = function () {
@@ -46,4 +46,22 @@ $(document).ready(function () {
     $('#current').css('background-color', color);
   };
   $('#colors').on('click', '.box', set_color);
+
+  var add_image = function () {
+    var img = $('#image').val();
+    var $img = $('<img>').attr('src', img);
+
+    $('#images').prepend($img);
+    $('#image').val('').focus();
+  };
+  $('#add_image').click(add_image);
+
+  var set_image = function () {
+    var $img = $(this);
+    var img = $img.attr('src');
+
+    $('#current_image img').attr('src', img);
+    $('#canvas').css('background-image', 'url(' + img + ')');
+  };
+  $('#images').on('click', 'img', set_image);
 });
