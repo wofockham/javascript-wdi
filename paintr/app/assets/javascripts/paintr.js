17790c5de87a635509022d4d894c70124482c324
Cold Head <certainthings@gmail.com>
2013-07-16 15:12:57 +1000

    Paintr v0.1

---
+++ paintr/app/assets/javascripts/paintr.js
@@ -0,0 +1,48 @@
+$(document).ready(function () {
+
+  var create_paint_boxes = function (boxes) {
+    for (var i = 0; i < boxes; i++) {
+      var $paint = $('<div/>').addClass('paint');
+      $('#canvas').append($paint);
+    }
+  };
+  create_paint_boxes(5000);
+
+  var paint_box = function () {
+    var $box = $(this);
+    var color = $('#current').css('background-color');
+    $box.css('background-color', color);
+  };
+  $('.paint').click(paint_box);
+
+  var clear_colors = function () {
+    $('#colors').empty();
+    $('#current').css('background-color', 'white');
+  }
+  $('#clear').click(clear_colors);
+
+
+  var add_color = function () {
+    var color = $('#color').val();
+
+    var $box = $('<div/>');
+    $box.addClass('box');
+    $box.css('background-color', color);
+    $box.text(color);
+
+    $('#colors').prepend($box);
+    $('#color').val('').focus();
+
+    //$box.click(set_color);
+  };
+  $('#add_color').click(add_color);
+
+
+  var set_color = function () {
+    var $box = $(this);
+    var color = $box.css('background-color');
+
+    $('#current').css('background-color', color);
+  };
+  $('#colors').on('click', '.box', set_color);
+});
---
+++ paintr/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Paintr::Application.config.secret_token = 'aac3ced76ee8294a6ea528f3ad5599254cb64e404b17c5766fa793d519e266e9cb1f3faadafcd27e9968c6359d22905454b369036754359971436811f88e655b'
