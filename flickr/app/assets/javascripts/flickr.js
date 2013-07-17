6d84ceca630465afa49d747f911964e12de7efdf
Cold Head <certainthings@gmail.com>
2013-07-17 15:35:40 +1000

    This is working

--- flickr/app/assets/javascripts/flickr.js
+++ flickr/app/assets/javascripts/flickr.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+
   var search_flickr = function () {
     var search = $('#search').val();
     var page = 1;
@@ -7,15 +8,27 @@ $(document).ready(function () {
     // http://farm4.static.flickr.com/3729/9303278039_e0a7479796_m.jpg
 
     var results = function (data) {
+      console.log('inside results');
       _.each(data.photos.photo, display_photo);
     };
 
     var display_photo = function (photo) {
+      var width = $('#width').val();
+      var height = $('#height').val();
+
       var url = "url(http://farm" + photo.farm + ".static.flickr.com/" + photo.server + "/" + photo.id + "_" + photo.secret + "_m.jpg)";
-      console.log(url);
+      var $image = $('<div>').addClass('image');
+      $image.css({
+        'background-image': url,
+        'width': width,
+        'height': height
+      });
+
+      $('#images').prepend($image);
     };
 
     $.getJSON('http://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=8f867234b61d2cbe2839b187ff4a203c&text=' + search + '&per_page=500&page=' + page + '&format=json&jsoncallback=?', results);
+    console.log('after getJSON');
   };
 
   $('#flickr').click(search_flickr);
