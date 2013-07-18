856f694dc13606b6386ac8ba960a67664ab067fb
Cold Head <certainthings@gmail.com>
2013-07-18 10:29:14 +1000

    Init, migrations

---
+++ todo/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Todo::Application.config.secret_token = 'e67c66c35f528e0b65d10c41ee1ef9b6174c95e2cd9fb4b64b06457d131ecde0081985217268a13fba38d0ef7a7166b06e897264ad9c3410ef909d51a9ee2fc9'
