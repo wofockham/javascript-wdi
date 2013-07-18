d791d372cfab12b2b6e81b4afb22920a9d88a21b
Cold Head <certainthings@gmail.com>
2013-07-18 12:15:58 +1000

    Sessiony stuff

--- todo/config/routes.rb
+++ todo/config/routes.rb
@@ -1,3 +1,8 @@
 Todo::Application.routes.draw do
   root :to => 'home#index'
+  resources :users, :only => [:index, :new, :create]
+
+  get '/login' => 'session#new'
+  post '/login' => 'session#create'
+  delete '/login' => 'session#destroy'
 end
