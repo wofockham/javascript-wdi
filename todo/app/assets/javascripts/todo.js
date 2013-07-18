3e05ccfadcd2a140ec8f8408edd10383999a7de0
Cold Head <certainthings@gmail.com>
2013-07-18 15:14:43 +1000

    New priority form JS

--- todo/app/assets/javascripts/todo.js
+++ todo/app/assets/javascripts/todo.js
@@ -10,5 +10,22 @@ $(document).ready(function () {
 
   create_boxes();
 
+  var create_priority = function () {
+    return false;
+  };
+
+  var show_form = function () {
+    $('#new_priority').hide();
+    $('.form').removeClass('invisible');
+  };
+
+  var hide_form = function () {
+    $('#new_priority').show();
+    $('.form').addClass('invisible');
+  }
+
+  $('#new_priority').click(show_form);
+  $('#create_priority').click(create_priority);
+  $('#cancel_priority').click(hide_form);
 });
 
