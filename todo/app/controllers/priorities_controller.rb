b329541dd10e39b52772e77966bd0bb1704dcfc4
Cold Head <certainthings@gmail.com>
2013-07-19 09:37:53 +1000

    Jerk version

--- todo/app/controllers/priorities_controller.rb
+++ todo/app/controllers/priorities_controller.rb
@@ -4,6 +4,14 @@ class PrioritiesController < ApplicationController
   end
 
   def create
-    binding.pry
+    color = params[:color]
+    name = params[:name]
+    value = params[:value]
+
+    # Fancy pants way for jerks:
+    # @auth.priorities.create(:color => color, :name => name, :value => value) # I can't believe this really works
+
+    priority = Priority.create(:color => color, :name => name, :value => value)
+    @auth.priorities << priority
   end
 end
