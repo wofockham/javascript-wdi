6541a73267d078087fdf61021296a03e993d7d7e
Cold Head <certainthings@gmail.com>
2013-07-23 13:08:01 +1000

    Javascript load order jiggery pokery

--- todo/app/assets/javascripts/todo.js
+++ todo/app/assets/javascripts/todo.js
@@ -211,23 +211,23 @@ $(document).ready(function () {
 var map;
 var canvas;
 
+var display_map = function (lat, long, zoom) {
+  var mapOptions = {
+    center: new google.maps.LatLng(lat, long),
+    zoom: zoom,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+  canvas = $('#map_canvas')[0];
+  map = new google.maps.Map(canvas, mapOptions);
+};
+
+var add_marker = function (lat, long, title) {
+  var latlng = new google.maps.LatLng(lat, long);
+  var marker = new google.maps.Marker({position: latlng, map: map, title: title});
+};
+
 $(document).ready(function () {
-  var display_map = function (lat, long, zoom) {
-    var mapOptions = {
-      center: new google.maps.LatLng(lat, long),
-      zoom: zoom,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-    canvas = $('#map_canvas')[0];
-    map = new google.maps.Map(canvas, mapOptions);
-  };
-
-  var add_marker = function (lat, long, title) {
-    var latlng = new google.maps.LatLng(lat, long);
-    var marker = new google.maps.Marker({position: latlng, map: map, title: title});
-  };
-
-  display_map(40.7316, -73.9901, 13);
+  display_map(-33.89336, 151.217167, 13);
 });
 
 
