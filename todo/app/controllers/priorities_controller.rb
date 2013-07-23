1f8f6e2457f46d08f7ebe491d7625b042d4cf101
Cold Head <certainthings@gmail.com>
2013-07-23 10:17:46 +1000

    Parallel assignment is awesome

--- todo/app/controllers/priorities_controller.rb
+++ todo/app/controllers/priorities_controller.rb
@@ -1,6 +1,6 @@
 class PrioritiesController < ApplicationController
   def index
-    @priorities = @auth.priorities
+    @priorities = @auth.priorities.order(:value).reverse
   end
 
   def create
@@ -25,34 +25,12 @@ class PrioritiesController < ApplicationController
 
   def up
     priority = Priority.find(params[:id])
-    higher = @auth.priorities.where('value > ?', priority.value).order('value ASC').first
-
-    if higher.present?
-      temp = priority.value
-      priority.value = higher.value
-      higher.value = temp
-      priority.save
-      higher.save
-      render :json => [priority, higher]
-    else
-      render :json => [priority]
-    end
+    render :json => priority.swap_higher(@auth)
   end
 
   def down
     priority = Priority.find(params[:id])
-    lower = @auth.priorities.where('value < ?', priority.value).order('value DESC').first
-
-    if lower.present?
-      temp = priority.value
-      priority.value = lower.value
-      lower.value = temp
-      priority.save
-      lower.save
-      render :json => [priority, lower]
-    else
-      render :json => [priority]
-    end
+    render :json => priority.swap_lower(@auth)
   end
 
 end
--- todo/app/models/priority.rb
+++ todo/app/models/priority.rb
@@ -15,4 +15,30 @@ class Priority < ActiveRecord::Base
   attr_accessible :name, :color, :value
   belongs_to :user, :inverse_of => :priorities
   has_many :tasks, :inverse_of => :priority
+
+  def swap_higher(user)
+    higher = user.priorities.where('value > ?', self.value).order('value ASC').first
+
+    if higher.present?
+      self.value, higher.value = higher.value, self.value
+      self.save
+      higher.save
+      [self, higher]
+    else
+      [self]
+    end
+  end
+
+  def swap_lower(user)
+    lower = user.priorities.where('value < ?', self.value).order('value DESC').first
+
+    if lower.present?
+      self.value, lower.value = lower.value, self.value
+      self.save
+      lower.save
+      [self, lower]
+    else
+      [self]
+    end
+  end
 end
