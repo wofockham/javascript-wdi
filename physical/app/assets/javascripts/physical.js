edb62af723176cbcf7193b9036b67549b9a398fa
Cold Head <certainthings@gmail.com>
2013-07-25 16:40:38 +1000

    charting v0.3

--- physical/app/assets/javascripts/physical.js
+++ physical/app/assets/javascripts/physical.js
@@ -9,8 +9,20 @@ $(document).ready(function () {
     }).done(process_activity);
   };
 
-  var process_activity = function () {
-
+  var process_activity = function (activities) {
+    new Morris.Line({
+      element: 'chart',
+      data: [
+        { year: '2008', value: 20 },
+        { year: '2009', value: 10 },
+        { year: '2010', value: 5 },
+        { year: '2011', value: 5 },
+        { year: '2012', value: 20 }
+      ],
+      xkey: 'year',
+      ykeys: ['value'],
+      labels: ['Value']
+    });
   };
 
   $('#show_chart').click(show_chart);
