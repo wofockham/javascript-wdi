db4b13fc33f89e232aabfa7290cfa681aa97e146
Cold Head <certainthings@gmail.com>
2013-07-25 11:37:00 +1000

    migrations, models, seeds

---
+++ physical/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Physical::Application.config.secret_token = 'b6c6e94d69874b27bef6806621a1e43f7faaeaddd34ba76b9247ea7abe96e97186b45fa93fcd6bffb7ffa292e7534ba0793c77d38b77e4083c57ffdd215e7958'
