af84a29a1a65617c401c941aa137011889ebe31a
Cold Head <certainthings@gmail.com>
2013-07-25 15:41:27 +1000

    exercise creation via ajax

--- physical/app/controllers/exercises_controller.rb
+++ physical/app/controllers/exercises_controller.rb
@@ -5,6 +5,16 @@ class ExercisesController < ApplicationController
     @exercises = @auth.exercises
   end
 
+  def new
+    @exercise = Exercise.new
+  end
+
+  def create
+    exercise = Exercise.create(params[:exercise])
+    @auth.exercises << exercise
+    @exercises = @auth.exercises
+  end
+
   private
   def only_authorized
     redirect_to(root_path) if @auth.nil?
