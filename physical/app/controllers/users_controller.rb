a30980eefff3b2b9e27cb4652e2416754f74704d
Cold Head <certainthings@gmail.com>
2013-07-25 14:44:49 +1000

    ajax user signup via partial madness

--- physical/app/controllers/users_controller.rb
+++ physical/app/controllers/users_controller.rb
@@ -2,4 +2,13 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    User.create(params[:user])
+    @users = User.order(:email)
+    respond_to do |format|
+      format.html { redirect_to(users_path) }
+      format.js { render :create }
+    end
+  end
 end
