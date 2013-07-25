3bbd83003ee1f45a6addd66d071b0cef45a676db
Cold Head <certainthings@gmail.com>
2013-07-25 12:17:16 +1000

    Thanks Daniel

--- physical/app/controllers/application_controller.rb
+++ physical/app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :authenticate
+
+  private
+  def authenticate
+    return unless session[:user_id]
+
+    @auth = User.find(session[:user_id])
+    unless @auth
+      session[:used_id] = nil
+      redirect_to root_url
+    end
+  end
 end
