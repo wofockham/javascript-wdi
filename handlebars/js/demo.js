07671f166bb0f7c830aa9e963af5982d7d90801d
Cold Head <certainthings@gmail.com>
2013-08-12 10:09:15 +1000

    handlebars, first page append

--- handlebars/js/demo.js
+++ handlebars/js/demo.js
@@ -1,1 +1,15 @@
-console.log('welcome to handlebars', $, Handlebars)
+$(document).ready(function () {
+
+  var post = {
+    title: 'Post title',
+    date: 'Yesterday',
+    body: 'Lorem impsum blah blah blah'
+  };
+
+  var template_str = $('#post_template').html(); // Fetch the HTML
+  var template_f = Handlebars.compile(template_str); // Turn it into a template function.
+
+  var new_post = template_f(post);
+
+  $('#posts').append(new_post);
+});
